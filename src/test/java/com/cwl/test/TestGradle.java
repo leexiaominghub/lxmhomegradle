f7cdb325a14dfc01b520b9ed6591a076af037976
lixiaoming-mac <leexiaoming@foxmail.com>
2020-12-12 11:34:05 +0800

    Initial commit

---
+++ src/test/java/com/cwl/test/TestGradle.java
@@ -0,0 +1,14 @@
+package com.cwl.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j
+public class TestGradle {
+
+    @Test
+    public void testPrint() {
+        log.info("testPrint");
+
+    }
+}
